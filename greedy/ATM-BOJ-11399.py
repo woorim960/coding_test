8cae21091f33f43ee227ece898cdaa3b206cfdbf
박우림 <woorimprog@gmail.com>
2020-09-14 07:28:11 +0900

    ATM 풀이 수정
    Signed-off-by: 박우림 <woorimprog@gmail.com>

--- greedy/ATM-BOJ-11399.py
+++ greedy/ATM-BOJ-11399.py
@@ -7,9 +7,5 @@
 n = int(input())
 ls = sorted(map(int, input().split()))
 
-total = [sum(ls[:i+1]) for i in range(n)]
-# 정렬된 줄의 순서대로 자기 차례의 합산된 값을 total에 더해준다.
-# for i in range(n) :
-#   total += sum(ls[:i+1])  
-
-print(sum(total))
+total = [sum(ls[:i+1]) for i in range(n)]   # 정렬된 줄의 순서대로 자기 차례까지의 값들을 합산하여 리스트에 저장
+print(sum(total))   # 모든 값들을 더한 후 출력
