bc9983e4604dac2174b26440fcc360f895669cfc
woorim960 <woorimprog@gmail.com>
2021-08-18 01:37:47 +0900

    동적 프로그래밍: 1로 만들기 (백준 1463번) 풀이

---
+++ dynamic-programing/1로-만들기(백준-1463번).py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10**9)
+
+def top_down(n):
+  if (n <= 1): return 0
+  if (n <= 3): return 1
+  if (d[n] != 0): return d[n]
+  
+  d[n] = top_down(n - 1) + 1
+  if (n % 2 == 0): d[n] = min(d[n], top_down(n // 2) + 1)
+  if (n % 3 == 0): d[n] = min(d[n], top_down(n // 3) + 1)
+  
+  return d[n]
+
+
+def bottom_up(n):
+  d = [0] * 1000001
+  d[0:6] = [0, 0, 1, 1, 2, 3]
+  
+  for i in range(6, n + 1):
+    d[i] = d[i - 1] + 1
+    if (i % 2 == 0): d[i] = min(d[i], d[i // 2] + 1)
+    if (i % 3 == 0): d[i] = min(d[i], d[i // 3] + 1)
+
+  return d[n]
+
+n = int(input())
+
+# d = [0] * 1000001
+# print(top_down(n))
+
+print(bottom_up(n))
