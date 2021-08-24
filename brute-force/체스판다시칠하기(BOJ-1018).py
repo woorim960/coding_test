5f1494389b815547e40390bcf64c856310cd2a65
woorim960 <woorimprog@gmail.com>
2021-08-25 03:50:24 +0900

    완전탐색: 체스판 다시 칠하기 '주석 추가'

--- brute-force/체스판다시칠하기(BOJ-1018).py
+++ brute-force/체스판다시칠하기(BOJ-1018).py
@@ -1,4 +1,9 @@
+N = list(map(int, input().split()))
+board = [input() for _ in range(N[0])]
+
+# 변환된 흑돌 갯수 카운팅
 def getCountChangedWhite(board, i, j):
+  # (i, j) 위치에서 8 x 8 크기의 체스판을 검사한다.
   count = 0
   for a in range(i, i + 8):
     for b in range(j, j + 8):
@@ -9,8 +14,9 @@ def getCountChangedWhite(board, i, j):
         count += 1
   return count
 
-
+# 변환된 흑돌 갯수 카운팅
 def getCountChangedBlack(board, i, j):
+  # (i, j) 위치에서 8 x 8 크기의 체스판을 검사한다.
   count = 0
   for a in range(i, i + 8):
     for b in range(j, j + 8):
@@ -21,8 +27,9 @@ def getCountChangedBlack(board, i, j):
         count += 1
   return count
 
+# 메인 솔루션
 def solution(N, board):
-  x, y = N[0] - 7, N[1] - 7
+  x, y = N[0] - 7, N[1] - 7 # 8 x 8 크기의 체스판만 검사하면 되므로, (x, y)는 각 행과 열의 -8 위치에서부터 검사하도록 해야한다. -8이 아닌 -7을 한 이유는 아래 range() 함수에서 -1까지 순회하기 때문.
 
   result = []
   for i in range(x):
@@ -32,8 +39,5 @@ def solution(N, board):
       result.append(min(wCount, bCount))
 
   return min(result)
-
-N = list(map(int, input().split()))
-board = [input() for _ in range(N[0])]
 
 print(solution(N, board))
