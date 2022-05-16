ecc9faec56a2932d4de3c169cb5402244af093df
woorim960 <woorimprog@gmail.com>
2022-05-16 23:45:43 +0900

    js(recursion): hanoi solution

--- recursion/하노이탑-백준1914.js
+++ recursion/하노이탑-백준1914.js
@@ -1,11 +1,10 @@
 // 참고 알고리즘 : https://youtu.be/AogMbfRwguk
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 function solution(N) {
   const K = (BigInt(1) << BigInt(N)) - BigInt(1);
-  console.log(String(K));
   if (N <= 20) console.log(hanoi(N, 1, 3));
 }
 
@@ -14,7 +13,7 @@ function hanoi(n, from, to) {
   if (n <= 1) {
     return `${from} ${to}\n`;
   } else {
-    let s = "";
+    let s = '';
     s += hanoi(n - 1, from, mid);
     s += hanoi(1, from, to);
     s += hanoi(n - 1, mid, to);
